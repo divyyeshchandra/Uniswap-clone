ce3b8909feb891dc0ee82823468e790d33b9faf0
divyyeshchandra <divyyesh.bvp23@gmail.com>
2023-09-12 11:40:04 +0530

    Second commit

--- uniswap-app/components/SwapField.js
+++ uniswap-app/components/SwapField.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import Selector from './Selector'
+import React from "react";
+import Selector from "./Selector";
 
 const SwapField = React.forwardRef(({ obj }, inputRef) => {
-  const { id, value = '', setValue, defaultValue, setToken, ignoreValue } = obj
+  const { id, value = "", setValue, defaultValue, setToken, ignoreValue } = obj;
 
   return (
-    <div className='flex items-center rounded-xl'>
+    <div className="flex items-center rounded-xl">
       <input
         ref={inputRef}
         className={getInputClassname()}
-        type={'number'}
+        type={"number"}
         value={value}
-        placeholder={'0.0'}
-        onChange={e => {
-          setValue(e.target.value)
+        placeholder={"0.0"}
+        onChange={(e) => {
+          setValue(e.target.value);
         }}
       />
-
       <Selector
         id={id}
         setToken={setToken}
@@ -24,13 +23,15 @@ const SwapField = React.forwardRef(({ obj }, inputRef) => {
         ignoreValue={ignoreValue}
       />
     </div>
-  )
+  );
 
   function getInputClassname() {
     let className =
-      ' w-full outline-none h-8 px-2 appearance-none text-3xl bg-transparent'
-    return className
+      " w-full outline-none h-8 px-2 appearance-none text-3xl bg-transparent";
+    return className;
   }
-})
+});
 
-export default SwapField
+SwapField.displayName = "SwapField";
+
+export default SwapField;
